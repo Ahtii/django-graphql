2c3f60c82be100df976068d4e7fd72a6eea1d92b
Ahtisham <ahtishamshafi9906@gmail.com>
2021-01-20 16:25:59 +0530

    initial commit

---
+++ stock/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=20)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    expire_date = models.DateField()
+    created_date = models.DateField()
+
+    class Meta:
+        
+        db_table='product'
