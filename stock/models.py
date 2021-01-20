566118f7fff034e4c37506b8252bcb7cabeda08e
Ahtisham <ahtishamshafi9906@gmail.com>
2021-01-21 01:30:15 +0530

    added more graphql query

--- stock/models.py
+++ stock/models.py
@@ -16,7 +16,7 @@ class Vehicle(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(decimal_places=2, max_digits=20)
     launch_date = models.DateField()
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, default=None)
+    vendor = models.ForeignKey(Vendor, related_name="vehicles", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- stock/schema.py
+++ stock/schema.py
@@ -1,19 +1,29 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Vehicle
+from .models import Vehicle, Vendor
+
+class VendorType(DjangoObjectType):
+    class Meta:
+        model = Vendor
+        fields = ("id", "name", "vehicles")       
 
 class VehicleType(DjangoObjectType):
     class Meta:
         model = Vehicle
-        fields = ("id","name", "price", "launch_date")
-
+        fields = ("id","name", "price", "launch_date", "vendor")
 
 class Query(graphene.ObjectType):
 
-    all_vehicles = graphene.List(VehicleType) 
+    all_vehicles = graphene.List(VehicleType)
+    vendor_by_name = graphene.Field(VendorType, name=graphene.String(required=True))
 
     def resolve_all_vehicles(root, info):
-        return Vehicle.objects.all()
+        return Vehicle.objects.select_related("vendor").all()
 
+    def resolve_vendor_by_name(root, info, name):
+        try:
+            return Vendor.objects.get(name=name)    
+        except Vendor.DoesNotExist:
+            return None    
 
 schema = graphene.Schema(query=Query)    
