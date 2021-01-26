09c8805606996bc840ff997b84c05d7e9412f180
Ahtisham <ahtishamshafi9906@gmail.com>
2021-01-26 22:36:18 +0530

    removed unnecessary code

--- stock/urls.py
+++ stock/urls.py
@@ -8,10 +8,6 @@ from stock.graphql.schema import schema
 
 urlpatterns = [    
     path('', views.index, name='index'),  
-    #path('graphql', GraphQLView.as_view(graphiql=True)),
-    #path('query', views.query_graphql, name='query')  
-    #path('query', CustomGraphQLView.as_view(), name="query")
-    #path('custom_graphql', views.CustomGraphQLView.as_view(), name='custom_graphql'),
-    #path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path('custom_graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, backend=GraphQLCustomCoreBackend())), name='custom_graphql'),
+    path('graphql', GraphQLView.as_view(graphiql=True)),
+    path('custom_graphql', views.CustomGraphQLView.as_view(), name='custom_graphql'),    
 ]
--- stock/views.py
+++ stock/views.py
@@ -14,61 +14,22 @@ def index(request):
 def clean_query(query):
     return query.replace("\n","")    
 
-def get_graphql_result_from(request):
-    # result = {}
-    # if request.method == 'POST':
+def get_graphql_result_from(request):    
     body_data = json.loads(request.body.decode("utf-8"))
     query = clean_query(body_data['query'])                
     result = schema.execute(query, context_value=request)                      
     result = result.data  
     return result
 
-# class CustomGraphQLView():
-#     pass
-    # def post(self, request):
-    #     result = None
-    #     return JsonResponse(result)    
+class CustomGraphQLView(GraphQLCustomCoreBackend, View):
 
-# def query_graphql(request):
-#     result = get_graphql_result_from(request)        
-#     return JsonResponse(result, safe=False) 
+    def __init__(self, executor=None):        
+        super(GraphQLCustomCoreBackend, self).__init__(executor)
+        self.execute_params['allow_subscriptions'] = True        
 
-# class CustomGraphQLView(GraphQLCustomCoreBackend, View):
-
-#     def __init__(self, executor=None):
-#         # type: (Optional[Any]) -> None
-#         super(GraphQLCustomCoreBackend, self).__init__(executor)
-#         self.execute_params['allow_subscriptions'] = True        
-# # class CustomGraphQLView(GraphQLView, View):    
-
-# #     def __init__(self, executor=None):
-# #         # type: (Optional[Any]) -> None        
-# #         super(GraphQLView, self).__init__()
-# #         self.graphiql = False        
-    
-#     def post(self, request):
-#         result = get_graphql_result_from(request)
-#         # result = None
-#         # data = self.parse_body(request)
-#         # print(data)
-#         return JsonResponse(result, safe=False)  
-
-# class Subscribe(GraphQLView):
-    
-    # def __init__(self):
-    #     super(graphiql=False, **kwargs).__init__()
-    # def __init__(self):
-    #     super().__init__()
-        
-    # def post(self, request, *args, **kwargs):
-    #     print(request.body)
-    #     super().parse_body(self, request)
-    #     result = super().execute_graphql_request(
-    #         self, request=request, query=query
-    #     )
-    #     print(result)
-        #result = {}get_graphql_result_from("subscription", request)
-        # return JsonResponse(result)  
+    def post(self, request):
+        result = get_graphql_result_from(request)        
+        return JsonResponse(result, safe=False)  
 
 
 
